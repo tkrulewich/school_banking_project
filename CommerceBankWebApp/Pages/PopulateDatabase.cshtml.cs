d115aefcd9110fce66d9f1c3f10742826b9bda96
Edward Krulewich <ekkyv6@mail.umkc.edu>
2021-10-04 22:46:27 -0500

    Made populate database link restricted. Cannot be accessed from a non admin account

--- CommerceBankWebApp/Pages/PopulateDatabase.cshtml.cs
+++ CommerceBankWebApp/Pages/PopulateDatabase.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using CommerceBankWebApp.Data;
 using CommerceBankWebApp.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -12,6 +13,7 @@ using SpreadsheetLight;
 
 namespace CommerceBankWebApp.Pages
 {
+    [Authorize(Roles = "admin")]
     public class PopulateDatabaseModel : PageModel
     {
         // will store all Transactions in the database for testing purposes (regardless of accountnumber)
