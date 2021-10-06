9abd7de2e00318f0ec18dca0aa70e613e64cbab9
Edward Krulewich <ekkyv6@mail.umkc.edu>
2021-10-06 00:27:03 -0500

    Populate database page now allows you to upload a file to select your own excel dummy data. Will crash on bad data at the moment

--- CommerceBankWebApp/Pages/PopulateDatabase.cshtml.cs
+++ CommerceBankWebApp/Pages/PopulateDatabase.cshtml.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using CommerceBankWebApp.Data;
 using CommerceBankWebApp.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
@@ -29,120 +30,145 @@ namespace CommerceBankWebApp.Pages
         }
 
         // returns a list of all transactions in the excel file
-        public async Task ReadExcelData(string fileName, string sheetName) {
+        public async Task ReadExcelData(System.IO.Stream file) {
             List<Transaction> transactionList = new List<Transaction>();
+            SLDocument document = new SLDocument(file);
 
-            SLDocument document = new SLDocument(fileName, sheetName);
+            var workSheets = document.GetSheetNames();
 
-            SLWorksheetStatistics stats = document.GetWorksheetStatistics();
 
-            for (short i = 2;i <= stats.EndRowIndex;i++)
+            foreach (var sheet in workSheets)
             {
-                string accountType = "";
-                long? accountNumber = null;
-                DateTime? processingDate = null;
-                double? balance = null;
-                bool? isCredit = null;
-                double? amount = null;
-                string description = "No desciption";
+                document.SelectWorksheet(sheet);
 
-                for (short j = 1;j <= 8;j++)
+                SLWorksheetStatistics stats = document.GetWorksheetStatistics();
+
+                for (short i = 2; i <= stats.EndRowIndex; i++)
                 {
-                    if (!document.HasCellValue(i, j)) continue;
+                    string accountType = "";
+                    long? accountNumber = null;
+                    DateTime? processingDate = null;
+                    double? balance = null;
+                    bool? isCredit = null;
+                    double? amount = null;
+                    string description = "No desciption";
 
-                    switch (document.GetCellValueAsString(1, j))
+                    for (short j = 1; j <= 8; j++)
                     {
-                        case "Account Type":
-                            accountType = document.GetCellValueAsString(i, j);
-                            break;
-                        case "Acct #":
-                            accountNumber = document.GetCellValueAsInt64(i, j);
-                            break;
-                        case "Processing Date":
-                            processingDate = document.GetCellValueAsDateTime(i, j);
-                            break;
-                        case "Balance":
-                            balance = document.GetCellValueAsDouble(i, j);
-                            break;
-                        case "CR (Deposit) or DR (Withdrawal":
-                        case "CR (Deposit) or DR (Withdrawal)":
-                            string cellText = document.GetCellValueAsString(i, j);
+                        if (!document.HasCellValue(i, j)) continue;
 
-                            if (cellText == "CR") isCredit = true;
-                            else isCredit = false;
-                            break;
-                        case "Amount":
-                            amount = document.GetCellValueAsDouble(i, j);
-                            break;
-                        case "Description 1":
-                            description = document.GetCellValueAsString(i, j);
-                            break;
-                    }
-                }
-
-
-                if (accountNumber.HasValue && processingDate.HasValue && isCredit.HasValue && amount.HasValue)
-                {
-                    var bankAccountQuery = await _context.BankAccounts.Where(ac => ac.AccountNumber == accountNumber.Value).ToListAsync();
-
-                    BankAccount bankAccount;
-
-                    if (!bankAccountQuery.Any())
-                    {
-                        if (String.IsNullOrEmpty(accountType)) accountType = "Checking";
-
-                        bankAccount = new BankAccount()
+                        switch (document.GetCellValueAsString(1, j))
                         {
-                            AccountNumber = accountNumber.Value,
-                            AccountType = accountType,
-                            Transactions = new List<Transaction>()
-                        };
+                            case "Account Type":
+                                accountType = document.GetCellValueAsString(i, j);
+                                break;
+                            case "Acct #":
+                                accountNumber = document.GetCellValueAsInt64(i, j);
+                                break;
+                            case "Processing Date":
+                                processingDate = document.GetCellValueAsDateTime(i, j);
+                                break;
+                            case "Balance":
+                                balance = document.GetCellValueAsDouble(i, j);
+                                break;
+                            case "CR (Deposit) or DR (Withdrawal":
+                            case "CR (Deposit) or DR (Withdrawal)":
+                                string cellText = document.GetCellValueAsString(i, j);
 
-                        await _context.BankAccounts.AddAsync(bankAccount);
-
-                        await _context.SaveChangesAsync();
-
-                        bankAccountQuery = await _context.BankAccounts.Where(ac => ac.AccountNumber == accountNumber.Value).ToListAsync();
-
-                        bankAccount = bankAccountQuery.First();
-                    } else
-                    {
-                        bankAccount = bankAccountQuery.First();
-                        if (accountType != bankAccount.AccountType)
-                        {
-                            _logger.LogWarning($"Account type wrong. Account exists already with type: {accountType} but excel data claims type {bankAccountQuery.First().AccountType}");
-                            accountType = bankAccountQuery.First().AccountType;
+                                if (cellText == "CR") isCredit = true;
+                                else isCredit = false;
+                                break;
+                            case "Amount":
+                                amount = document.GetCellValueAsDouble(i, j);
+                                break;
+                            case "Description 1":
+                                description = document.GetCellValueAsString(i, j);
+                                break;
                         }
                     }
 
-                    _logger.LogInformation(description);
-                    Transaction transaction = new Transaction
+
+                    if (accountNumber.HasValue && processingDate.HasValue && isCredit.HasValue && amount.HasValue)
                     {
-                        ProcessingDate = processingDate.Value,
-                        Balance = balance,
-                        IsCredit = isCredit.Value,
-                        Amount = amount.Value,
-                        Description = description
-                    };
+                        var bankAccountQuery = await _context.BankAccounts.Where(ac => ac.AccountNumber == accountNumber.Value).Include(ac => ac.Transactions).ToListAsync();
 
-                    bankAccount.Transactions.Add(transaction);
+                        BankAccount bankAccount;
 
-                    _context.BankAccounts.Attach(bankAccount);
+                        if (!bankAccountQuery.Any())
+                        {
+                            if (String.IsNullOrEmpty(accountType)) accountType = "Checking";
 
-                    await _context.SaveChangesAsync();
+                            bankAccount = new BankAccount()
+                            {
+                                AccountNumber = accountNumber.Value,
+                                AccountType = accountType,
+                                Transactions = new List<Transaction>()
+                            };
+
+                            await _context.BankAccounts.AddAsync(bankAccount);
+
+                            await _context.SaveChangesAsync();
+
+                            bankAccountQuery = await _context.BankAccounts.Where(ac => ac.AccountNumber == accountNumber.Value).Include(ac => ac.Transactions).ToListAsync();
+
+                            bankAccount = bankAccountQuery.First();
+                        }
+                        else
+                        {
+                            bankAccount = bankAccountQuery.First();
+                            if (accountType != bankAccount.AccountType)
+                            {
+                                _logger.LogWarning($"Account type wrong. Account exists already with type: {accountType} but excel data claims type {bankAccountQuery.First().AccountType}");
+                                accountType = bankAccountQuery.First().AccountType;
+                            }
+                        }
+
+                        _logger.LogInformation(description);
+                        Transaction transaction = new Transaction()
+                        {
+                            ProcessingDate = processingDate.Value,
+                            Balance = balance,
+                            IsCredit = isCredit.Value,
+                            Amount = amount.Value,
+                            Description = description
+                        };
+
+                        bankAccount.Transactions.Add(transaction);
+
+                        _context.BankAccounts.Attach(bankAccount);
+
+                        await _context.SaveChangesAsync();
+                    }
                 }
             }
 
         }
 
+
+
         public async Task<IActionResult> OnGetAsync()
         {
             // add all transactions in the excel file to the database
-            await ReadExcelData("transaction_data.xlsx", "Cust A");
-            await ReadExcelData("transaction_data.xlsx", "Cust B");
+            //await ReadExcelData("transaction_data.xlsx", "Cust A");
+            //await ReadExcelData("transaction_data.xlsx", "Cust B");
 
             // save the changes
-            await _context.SaveChangesAsync();
+            //await _context.SaveChangesAsync();
+
+            BankAccounts = await _context.BankAccounts.Include(b => b.Transactions).ToListAsync();
+
+            return Page();
+        }
+
+        [BindProperty]
+        public IFormFile Upload { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            _logger.LogInformation(Upload.FileName);
+
+
+            await ReadExcelData(Upload.OpenReadStream());
 
             BankAccounts = await _context.BankAccounts.Include(b => b.Transactions).ToListAsync();
 
