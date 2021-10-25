ff649f0fe544efee40ee35af006aab156e42406b
AlexJNorris <a.norris97@icloud.com>
2021-10-25 01:24:57 -0500

    Add landing page widget

--- CommerceBankWebApp/Pages/Index.cshtml.cs
+++ CommerceBankWebApp/Pages/Index.cshtml.cs
@@ -2,15 +2,57 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CommerceBankWebApp.Data;
+using CommerceBankWebApp.Models;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
 
 namespace CommerceBankWebApp.Pages
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        public List<BankAccount> BankAccounts { get; set; }
+        public BankAccount AccountToDisplay { get; set; }
+
+        private readonly ApplicationDbContext _context;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        // The constructor enables logging and access to the database
+        public IndexModel(ApplicationDbContext context,
+            SignInManager<IdentityUser> signInManager,
+            UserManager<IdentityUser> userManager)
         {
+            _context = context;
+            _signInManager = signInManager;
+            _userManager = userManager;
+
+            BankAccounts = new List<BankAccount>();
+        }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            if (_signInManager.IsSignedIn(User))
+            {
+                // if the user is an admin, read ALL bank accounts
+                if (User.IsInRole("admin"))
+                {
+                    BankAccounts = await _context.GetAllBankAccountsWithTransactions();
+                }
+                // otherwise just read the accounts that belong to the user
+                else
+                {
+                    // get users info
+                    var userId = _userManager.GetUserId(User);
+                    BankAccounts = await _context.GetAllBankAccountsFromUserWithTransactions(userId);
+                }
+            }
+            return Page();
         }
     }
 }
