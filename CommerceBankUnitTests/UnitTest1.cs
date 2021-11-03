9e422d87ce27dd35c58c3fe55ac48a6ef22db3b6
Edward Krulewich <ekkyv6@mail.umkc.edu>
2021-11-02 23:08:06 -0500

    unit testing change

--- CommerceBankUnitTests/UnitTest1.cs
+++ CommerceBankUnitTests/UnitTest1.cs
@@ -5,51 +5,28 @@ using System;
 using System.Linq;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Identity;
+using CommerceBankWebApp.Data;
+using CommerceBankWebApp.Models;
 
 namespace CommerceBankUnitTests
 {
     [TestClass]
     public class UnitTest1
     {
+        private readonly ApplicationDbContext _context;
 
         string accountNumber = "78907623";
 
         [TestMethod]
-        public void GetAllBankAccountsFromUserTest()
+        public async void GetAllBankAccountsFromUserTest()
         {
-            using (var db = new CommerceBankWebApp.Data.ApplicationDbContext(new DbContextOptionsBuilder<CommerceBankWebApp.Data.ApplicationDbContext>().UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=aspnet-CommerceBankWebApp-43D51693-8F5B-4F63-855F-FE733AE31BA6;Trusted_Connection=True;MultipleActiveResultSets=true").Options))
-            {
-                Assert.AreNotEqual(db.GetAllBankAccountsFromUser(accountNumber).Result.Count, 0);
-            }
-        }
-        [TestMethod]
-        public void RegisterTest()
-        {
-            using (var db = new CommerceBankWebApp.Data.ApplicationDbContext(new DbContextOptionsBuilder<CommerceBankWebApp.Data.ApplicationDbContext>().UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=aspnet-CommerceBankWebApp-43D51693-8F5B-4F63-855F-FE733AE31BA6;Trusted_Connection=True;MultipleActiveResultSets=true").Options))
-            {
-                var user = new IdentityUser
-                {
-                    UserName = "Alm@jklsdja.com",
-                    Email = "Alm@jklsdja.com"
+            var builder = new DbContextOptionsBuilder<ApplicationDbContext>()
+    .UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=aspnet-CommerceBankWebApp-43D51693-8F5B-4F63-855F-FE733AE31BA6;Trusted_Connection=True;MultipleActiveResultSets=true");
 
-                };
-                var accountHolder = new CommerceBankWebApp.Models.AccountHolder()
-                {
-                    FirstName = "Bob",
-                    LastName = "Daly",
-                    DateOfBirth = DateTime.Today,
-                    EmailAddress = "Alm@jklsdja.com",
-                    PhoneNumber = "9090009999",
-                    DateBecameCustomer = DateTime.Today,
-                    BankAccounts = new List<CommerceBankWebApp.Models.BankAccount>(),
-                };
-
-                CommerceBankWebApp::CreateAsync();
-                db.Users.Create(user, "ghjsaldgdjsa8");
-                db.AddNewBankAccount(accountHolder, user, accountNumber, CommerceBankWebApp.Models.BankAccountType.Checking.Id, 8034982);
-                List<CommerceBankWebApp.Models.BankAccount> bankAccounts = db.GetAllBankAccountsFromUser(accountHolder.WebAppUserId).Result;
-                Assert.AreNotEqual(bankAccounts.Count, 0);
-            }
+            ApplicationDbContext _context  = new ApplicationDbContext(builder.Options);
+            List<BankAccount> acccounts = await _context.GetAllBankAccounts();
+            Assert.Equals(acccounts.Count(), 0);
+            
         }
     }
 }
